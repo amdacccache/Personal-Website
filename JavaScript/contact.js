af638c8bf0c5d0d7e2a55d481c30d8065e356105
amdaccache <daccache.a@husky.neu.edu>
2021-02-15 18:49:18 -0800

    first commit

---
+++ JavaScript/contact.js
@@ -0,0 +1,18 @@
+const inputs = document.querySelectorAll('.input');
+
+function focusFunc() {
+  const parent = this.parentNode;
+  parent.classList.add('focus');
+}
+
+function blurFunc() {
+  const parent = this.parentNode;
+  if (this.value === '') {
+    parent.classList.remove('focus');
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener('focus', focusFunc);
+  input.addEventListener('blur', blurFunc);
+});
